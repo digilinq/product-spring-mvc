c829e62ed0741b88fbb5f0bd7146a2368bea7cd5
Hossein Mohammadi <hossein.mohammadi@ah.nl>
2024-03-15 18:12:09 +0100

    Add mongodb dependency

---
+++ repository/src/test/java/com/eightbits/commerce/retail/TestConfiguration.java
@@ -0,0 +1,9 @@
+package com.eightbits.commerce.retail;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+public class TestConfiguration {
+}
